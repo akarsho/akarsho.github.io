28539743291161c799d3ec3a6c836292f07cb431
Akarsh Ojha <akarsho133@gmail.com>
2021-07-18 18:13:39 -0700

    Backup

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-
-let scrollAbout = () => {
-    document.getElementById("aboutMe").scrollIntoView({behavior:"smooth", block:"end"});
-}
-
-document.getElementById("about").addEventListener("click", scrollAbout);
-
 let loadGit = () => {
     window.open("https://github.com/akarsho");
 }
