b5c1eb6c4024eecc89a0c224d052050a2767604f
Akarsh Ojha <akarsho133@gmail.com>
2021-07-24 11:23:25 -0700

    Backup 7/24

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ let loadGit = () => {
 let loadLinkedin = () => {
     window.open("https://www.linkedin.com/in/akarsh-ojha-53a218217/")
 }
+
+let scrollAbout = () => {
+    document.getElementById("aboutme").scrollIntoView({behavior:"smooth", block:"end"});
+    console.log("YOY");
+}
+
+document.getElementById("about").addEventListener("click", scrollAbout);
